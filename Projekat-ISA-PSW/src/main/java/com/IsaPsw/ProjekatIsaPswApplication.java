41f7094796e237a8662708d37183a0dc622e1b7a
user <user@DESKTOP-G6V0CUS>
2019-11-06 21:40:33 +0100

    first commit

---
+++ Projekat-ISA-PSW/src/main/java/com/IsaPsw/ProjekatIsaPswApplication.java
@@ -0,0 +1,13 @@
+package com.IsaPsw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjekatIsaPswApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjekatIsaPswApplication.class, args);
+	}
+
+}
