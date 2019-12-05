e62125ee08ed3bfbb46b4432e57564abbd8fd73a
urbanaleksandra <urb.saska@gmail.com>
2019-12-05 03:16:29 +0100

    patient-profile-fix

--- Projekat-ISA-PSW/src/main/java/com/contoller/PatientContoller.java
+++ Projekat-ISA-PSW/src/main/java/com/contoller/PatientContoller.java
@@ -39,9 +39,42 @@ public class PatientContoller {
 		return new ResponseEntity<>(user, HttpStatus.OK);
 
 	}
+
+	@CrossOrigin(origins = "http://localhost:4200")
+	@RequestMapping(value="/changePatientInfo", method=RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody ResponseEntity<Patient> changePatientInfo(@RequestBody Patient patientNovi){
+
+		Patient patient1 = patientService.findByUsername(patientNovi.getUsername());
+		if(patient1 != null){
+			patient1.setFirstName(patientNovi.getFirstName());
+			patient1.setLastName(patientNovi.getLastName());
+			patient1.setCity(patientNovi.getCity());
+			patient1.setCountry(patientNovi.getCountry());
+			patient1.setAddress(patientNovi.getAddress());
+			patient1.setMobileNumber(patientNovi.getMobileNumber());
+
+			patientService.save(patient1);
+		}
+		else{
+			Patient patient = new Patient(patientNovi.getUsername(), patientNovi.getPassword(), patientNovi.getFirstName(), patientNovi.getLastName(), patientNovi.getEmail(), patientNovi.getAddress(), patientNovi.getCity(), patientNovi.getCountry(), patientNovi.getMobileNumber(), patientNovi.getJmbg());
+
+			patientService.save(patient);
+		}
+		Patient patient3 = new Patient();
+		return new ResponseEntity<>(patient3, HttpStatus.OK);
+
+	}
+
 	@CrossOrigin
 	@GetMapping("pacijenti")
 	public List<Patient> getPatients(){
 		return patientService.findAll();
+	}
+
+	@CrossOrigin(origins = "http://localhost:4200")
+	@RequestMapping(value="/pacijent/{username}", method= RequestMethod.GET)
+	public Patient getPatient(@PathVariable String username){
+
+		return patientService.findByUsername(username);
 	}
 }
