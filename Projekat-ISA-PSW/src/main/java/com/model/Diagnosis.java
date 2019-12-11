3d746a80b548c3b8649c2cd2d09347266cb3c407
Tamara Jovicic <57210311+tacajo@users.noreply.github.com>
2019-12-11 21:38:04 +0100

    Added class Diagnosis to Model

---
+++ Projekat-ISA-PSW/src/main/java/com/model/Diagnosis.java
@@ -0,0 +1,44 @@
+package com.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Diagnosis {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    public Diagnosis() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
