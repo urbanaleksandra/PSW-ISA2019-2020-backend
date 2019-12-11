b63404962206050e51f1c8ad1cac8c21bfa486c4
zeljana2309 <57210469+zeljana2309@users.noreply.github.com>
2019-12-11 16:07:59 +0100

    Added classes for HospitalRoom controller and service.

---
+++ Projekat-ISA-PSW/src/main/java/com/contoller/HospitalRoomController.java
@@ -0,0 +1,9 @@
+package com.contoller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+public class HospitalRoomController {
+}
