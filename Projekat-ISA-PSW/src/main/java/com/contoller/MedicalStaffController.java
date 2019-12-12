660ab6dbdbb7ea7fce55fd5d530395f47352629f
zeljana2309 <57210469+zeljana2309@users.noreply.github.com>
2019-12-12 01:16:43 +0100

    change of attribute firstName is done

--- Projekat-ISA-PSW/src/main/java/com/contoller/MedicalStaffController.java
+++ Projekat-ISA-PSW/src/main/java/com/contoller/MedicalStaffController.java
@@ -1,8 +1,12 @@
 package com.contoller;
 
+import com.model.Doctor;
 import com.model.MedicalStaff;
+import com.model.Patient;
 import com.service.MedicalStaffService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "http://localhost:4200")
@@ -17,5 +21,23 @@ public class MedicalStaffController {
     public MedicalStaff getMedStaff(@PathVariable String username){
 
         return medicalStaffService.findByUsername(username);
+    }
+
+    @CrossOrigin(origins = "http://localhost:4200")
+    @RequestMapping(value="/osobljePromjena", method= RequestMethod.POST)
+    public @ResponseBody ResponseEntity<Doctor> changeInfo(@RequestBody Doctor mdNovi){
+        Doctor md = (Doctor) medicalStaffService.findByUsername(mdNovi.getUsername());
+        if(md != null){
+            md.setFirstName(mdNovi.getFirstName());
+
+            medicalStaffService.save(md);
+        }
+        else{
+           // Patient patient = new Patient(patientNovi.getUsername(), patientNovi.getPassword(), patientNovi.getFirstName(), patientNovi.getLastName(), patientNovi.getEmail(), patientNovi.getAddress(), patientNovi.getCity(), patientNovi.getCountry(), patientNovi.getMobileNumber(), patientNovi.getJmbg());
+
+           // patientService.save(patient);
+        }
+        return new ResponseEntity<>(md, HttpStatus.OK);
+
     }
 }
