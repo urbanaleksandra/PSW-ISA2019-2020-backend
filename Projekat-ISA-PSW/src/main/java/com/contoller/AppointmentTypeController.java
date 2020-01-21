6404acae0fff95dc257ab439e602bb5710fd5dd4
zeljana2309 <57210469+zeljana2309@users.noreply.github.com>
2020-01-22 00:46:28 +0100

    deleting and changing appointment types

--- Projekat-ISA-PSW/src/main/java/com/contoller/AppointmentTypeController.java
+++ Projekat-ISA-PSW/src/main/java/com/contoller/AppointmentTypeController.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.model.AppointmentType;
 import com.service.AppointmentTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -29,6 +32,40 @@ public class AppointmentTypeController {
     public List<AppointmentType> getTypes(){
 
         return appointmentTypeService.findAll();
+    }
+
+    @CrossOrigin(origins = "http://localhost:4200")
+    @RequestMapping(value="/delete-type", method=RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
+    public  @ResponseBody
+    ResponseEntity<AppointmentType> deleteType(@RequestBody AppointmentType type){
+
+        System.out.println(type.getName());
+        AppointmentType hr=appointmentTypeService.findByName(type.getName());
+        appointmentTypeService.delete(hr);
+        AppointmentType hRoom = new AppointmentType(type.getName());
+
+
+        return new ResponseEntity<>(hRoom, HttpStatus.OK);
+    }
+
+
+    @CrossOrigin(origins = "http://localhost:4200")
+    @RequestMapping(value="/changeTypeInfo/{name}", method=RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody ResponseEntity<AppointmentType> changeInfo(@RequestBody AppointmentType type, @PathVariable String name){
+
+        AppointmentType type1 = appointmentTypeService.findByName(name);
+        if(type1 != null){
+            type1.setName(type.getName());
+            appointmentTypeService.save(type1);
+        }
+        else{
+            AppointmentType type2=new AppointmentType();
+            type2.setName(type.getName());
+            appointmentTypeService.save(type2);
+        }
+        AppointmentType type3 = new AppointmentType();
+        return new ResponseEntity<>(type3, HttpStatus.OK);
+
     }
 
 }
