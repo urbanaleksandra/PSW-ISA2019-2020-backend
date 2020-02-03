d67dc98d5f2444893c972be83a312015fb8f27ab
urbanaleksandra <urb.saska@gmail.com>
2020-02-03 19:25:45 +0100

    bugfix

--- Projekat-ISA-PSW/src/main/java/com/dto/AppointmentDTO.java
+++ Projekat-ISA-PSW/src/main/java/com/dto/AppointmentDTO.java
@@ -12,14 +12,6 @@ public class AppointmentDTO {
 	//private RecipeDTO recipe;
 
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
 	private String date;
 	
 	private String description;
