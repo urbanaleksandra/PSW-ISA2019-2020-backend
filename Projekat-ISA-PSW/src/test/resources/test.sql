ac5415c414d6f60678ddec76b942cf3b0fa4e15e
Tamara Jovicic <57210311+tacajo@users.noreply.github.com>
2020-02-08 04:18:41 +0100

    TESTS-app service

--- Projekat-ISA-PSW/src/test/resources/test.sql
+++ Projekat-ISA-PSW/src/test/resources/test.sql
@@ -7,13 +7,13 @@ CREATE SEQUENCE medical_staff_id_seq START WITH 1 INCREMENT BY 1;
 CREATE SEQUENCE medical_record_id_seq START WITH 1 INCREMENT BY 1;
 CREATE SEQUENCE hospital_room_id_seq START WITH 1 INCREMENT BY 1;
 CREATE SEQUENCE surgery_id_seq START WITH 1 INCREMENT BY 1;
-CREATE SEQUENCE medical_record_surgeries_id_seq START WITH 1 INCREMENT BY 1;
 CREATE SEQUENCE appointments_id_seq START WITH 1 INCREMENT BY 1;
 CREATE SEQUENCE appointment_type_id_seq START WITH 1 INCREMENT BY 1;
 CREATE SEQUENCE drug_id_seq START WITH 1 INCREMENT BY 1;
 CREATE SEQUENCE recipe_id_seq START WITH 1 INCREMENT BY 1;
 CREATE SEQUENCE diagnosis_id_seq START WITH 1 INCREMENT BY 1;
 CREATE SEQUENCE price_list_id_seq START WITH 1 INCREMENT BY 1;
+create sequence request_appointments_id_seq START WITH 1 INCREMENT BY 1;
 
 
 INSERT INTO public.clinic (id,address, description, name, pricelist, profit, rating,longitude,lat) VALUES (nextval('clinic_id_seq'),'Koste Todorovica 26, 11129 Beograd', 'Klinika za pulmologiju je zdravstvena ustanova za plućne bolesti u sastavu Kliničkog centra Srbije.', 'Klinika za pulmologiju', 0, 0, 0,44.797860, 20.458775);
@@ -25,5 +25,14 @@ INSERT INTO public.clinic (id,address, description, name, pricelist, profit, rat
 INSERT INTO public.clinical_center_administrator (id, username, password, email, first_name, last_name, jmbg, address, city, country, mobile_number, first_log) VALUES (nextval('clinical_center_administrator_id_seq'),'tacajo', '123', 'tacajovicic@gmail.com', 'Tamara', 'Jovicic', '1401998', 'Bulevar Oslobodjenja 150', 'Novi Sad', 'Serbia', '6551585', 1);
 INSERT INTO public.clinic_administrator (id,username, password, email, first_name, last_name, jmbg, address, city, country, mobile_number, clinic_id) VALUES (nextval('clinic_administrator_id_seq'),'cadmin', 'cadmin', 'zeki.sipovac@gmail.com', 'Zeljana', 'Sipovac', '354168465', 'Glavna 54', 'Nevesinje', 'Bosna i Hercegovina', '0645865455', 1);
 
+INSERT INTO public.appointment_type(id,name) VALUES (nextval('appointment_type_id_seq'),'Kardioloski');
 
-INSERT INTO public.appointments (id, version, date, description, duration, patient,finished, type, doctor_id, hospital_room_id, medical_record_id, doctor_username,type2_id) VALUES (nextval('appointments_id_seq'),0,'2020-02-05T16:00', 'cold', 2, 'anaM', false,'appointment', null, null, null, 'doctor',null);
+INSERT INTO public.medical_staff (id, medical_staff_type, username, password, email, first_name, last_name, jmbg, address, city, country, mobile_number, review, role,pocetak_radnog_vremena,kraj_radnog_vremena) VALUES (nextval('medical_staff_id_seq'),'2', 'nurse', '123', 'tacajovicic@gmail.com', 'Ivana', 'Petrovic', '315787', 'Bulevar Oslobodjenja 103', 'Novi Sad', 'Serbia', '06658651', 2, 'nurse','07:00','14:00');
+INSERT INTO public.medical_staff (id,medical_staff_type, username, password, email, first_name, last_name, jmbg, address, city, country, mobile_number, review, role, clinic_id,pocetak_radnog_vremena,kraj_radnog_vremena, appointment_type_id) VALUES (nextval('medical_staff_id_seq'),'1', 'doctor', '123', 'tacajovicic@gmail.com', 'Djordje', 'Rakic', '6872368', 'Bulevar Kralja Petra I 2', 'Beograd', 'Serbia', '0612645665', 1, 'doctor', 1,'08:00','15:00', 1);
+
+INSERT INTO public.patient (id, address, city, country, email, first_name, jmbg, last_name, mobile_number, password, username, record_id, is_enabled) VALUES (nextval('patient_id_seq'),'Beogradska 6', 'Bileca', 'Bosnia and Herzegovina', 'tacajovicic@gmail.com', 'Marija', 1497,'Gutic', '0640589536', 'masa', 'masa', null, true);
+INSERT INTO public.medical_record (id, blood_type, diopter, height, weight, patient_id) VALUES (nextval('medical_record_id_seq'),'A', '+2.25', '185', '95',1);
+UPDATE public.patient SET record_id = 1 where id = 1;
+INSERT INTO public.hospital_room (name,room_number,clinic_id,version) VALUES ('Glavna sala',1, 1,0);
+INSERT INTO public.appointments (id, version, date, description, duration, patient,finished, type, doctor_id, hospital_room_id, medical_record_id, doctor_username,type2_id) VALUES (nextval('appointments_id_seq'),0,'2020-02-05T16:00', 'cold', 2, 'masa', false,'appointment', 2, 1, 1, 'doctor',1);
+INSERT INTO public.request_appointments (id,date, description, duration, patient, type,clinic_id) VALUES (nextval('request_appointments_id_seq'),'2020-04-02T10:00', 'opis1', 2, 'markoM', 'tip1',1);
